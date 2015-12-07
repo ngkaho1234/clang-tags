56d70650a3fcbcfbfe8bface38c2404d549aa2a7
Norman Ramsey <nr@cs.tufts.edu>
2015-12-07 17:16:52 -0500

    look for libclang in /usr/lib/llvm-3.5
    If I knew more Cmake, I'd have it search all
    versions from 3.2 to 3.5.  But I don't know
    how to get Cmake to do that.

--- cmake/Modules/FindLibclang.cmake
+++ cmake/Modules/FindLibclang.cmake
@@ -1,10 +1,10 @@
 find_path (Libclang_INCLUDE_DIR clang-c/Index.h
   HINTS "${LIBCLANG_ROOT}/include"
-  PATH  "/usr/lib/llvm-3.2/include")
+  PATH  "/usr/lib/llvm-3.5/include")
 
 find_library (Libclang_LIBRARY clang
   HINTS "${LIBCLANG_ROOT}/lib"
-  PATH  "/usr/lib/llvm-3.2/lib")
+  PATH  "/usr/lib/llvm-3.5/lib")
 
 set (Libclang_LIBRARIES    ${Libclang_LIBRARY})
 set (Libclang_INCLUDE_DIRS ${Libclang_INCLUDE_DIR})
